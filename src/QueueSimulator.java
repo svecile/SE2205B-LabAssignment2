9341506845c7aa9f3c3252e466ee1c74ccf71974
Spencer <svecile@uwo.ca>
2019-03-06 17:40:44 -0500

    mostly done just wrong output

--- src/QueueSimulator.java
+++ src/QueueSimulator.java
@@ -1,9 +1,11 @@
 import java.lang.*;
 
+import static java.lang.Math.abs;
+
+
 public class QueueSimulator {
     public enum Event {ARRIVAL, DEPARTURE}
 
-    ;
     private double currTime;
     private double arrivalRate;
     private double serviceTime;
@@ -23,14 +25,52 @@ public class QueueSimulator {
     }
 
     public QueueSimulator(double aR, double servT, double simT) {
+        arrivalRate = aR;
+        serviceTime = servT;
+        totalSimTime = simT;
+        timeForNextArrival = getRandTime(aR);
+        timeForNextDeparture = servT + timeForNextArrival;
     }
 
     public double calcAverageWaitingTime() {
-        return 1;
+        Data packet;
+        double sum = 0, count = eventQueue.size();
+
+        while (!eventQueue.isEmpty()) {
+            packet = eventQueue.dequeue();
+            sum += packet.getDepartureTime() - packet.getArrivalTime();
+        }
+        return sum / count;
+        //(1 / arrivalRate) * ((arrivalRate * serviceTime) + ((0.5 * ((arrivalRate * serviceTime) * (arrivalRate * serviceTime))) / (1 - (arrivalRate * serviceTime))));
     }
 
     public double runSimulation() {
-        return 1;
+        /*Packets are enqueued into buffer in the form Node.
+        The arrival time of the packet into buffer is recorded in the arrivalTime member of the Data variable
+        which is the E member of the Node struct.
+        When a packet is dequeued from buffer, its departure time is recorded in the departureTime member of the Data
+        variable and this node is then enqueued into the eventQueue queue which record all packets departing the buffer queue
+
+        When an event occurs, the simulator updates the current time variable currTime to the time at which the event occurs.*/
+
+        while (currTime < totalSimTime) {
+            Data packet = new Data();
+            if (buffer.isEmpty() || timeForNextArrival < timeForNextDeparture) {
+                //an arrival is happening
+                currTime += timeForNextArrival;
+                packet.setArrivalTime(currTime);
+                buffer.enqueue(packet);
+                timeForNextArrival = getRandTime(arrivalRate);
+
+            } else {
+                //a departure is happening
+                currTime += timeForNextDeparture;
+                packet = buffer.dequeue();
+                packet.setDepartureTime(currTime);
+                eventQueue.enqueue(packet);
+            }
+        }
+        return calcAverageWaitingTime();
     }
 }
 

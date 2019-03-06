ccaa3624237a00f47bd17d7d22c0d99b984a47b2
Spencer <svecile@uwo.ca>
2019-03-06 14:08:00 -0500

    added files

---
+++ src/Data.java
@@ -0,0 +1,19 @@
+public class Data {
+    double arrivalTime;
+    double departureTime;
+
+    public Data() {
+    }
+
+    public void setArrivalTime(double a) {
+    }
+
+    public void setDepartureTime(double d) {
+    }
+
+    public double getDepartureTime() {
+    }
+
+    public double getArrivalTime() {
+    }
+}
---
+++ src/LinkedListQueue.java
@@ -0,0 +1,19 @@
+public class LinkedListQueue<E> implements Queue<E> {
+    public LinkedListQueue() {
+    }
+
+    public int size() {
+    }
+
+    public boolean isEmpty() {
+    }
+
+    public E first() {
+    }
+
+    public void enqueue(E node) {
+    }
+
+    public E dequeue() {
+    }
+}
